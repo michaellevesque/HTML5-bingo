1a447b77d2f5235cb8dd74b5a0b6c0807d76407c
Jeff Hobbs <jeffehobbs@gmail.com>
2012-06-25 07:33:49 -0400

    First commit

---
+++ js/data.js
@@ -0,0 +1,32 @@
+
+var headerText = "Bingo";
+
+var footerText = "All Rights Reserved";
+
+var JSONBingo = {"squares": [
+        {"square": "one"},
+        {"square": "two"},
+        {"square": "three"},
+        {"square": "four"},
+        {"square": "five"},
+        {"square": "six"},
+        {"square": "seven"},
+        {"square": "eight"},
+        {"square": "nine"},
+        {"square": "ten"},
+        {"square": "eleven"},
+        {"square": "twelve"},
+        {"square": "thirteen"},
+        {"square": "fourteen"},
+        {"square": "fifteen"},
+        {"square": "sixteen"},
+        {"square": "seventeen"},
+        {"square": "eighteen"},
+        {"square": "nineteen"},
+        {"square": "twenty"},
+        {"square": "twenty one"},
+        {"square": "twenty two"},
+        {"square": "twenty three"},
+        {"square": "twenty four"}    	
+    ]
+};
---
+++ js/script.js
@@ -0,0 +1,106 @@
+
+$(document).ready(function() {
+	
+	$('body').on('touchmove', false);
+	
+	$('#header').append(headerText);
+	
+	var d = new Date();
+	var n = d.getFullYear()
+	$('#footer').append("&copy; "+n+" "+footerText);
+
+	var clickSnd = new Audio("audio/click.mp3");
+	var winSnd = new Audio("audio/win.mp3");
+
+	shuffle(JSONBingo.squares);
+	
+	for (i=0; i<24; i++)	{
+	
+		if (i==12) {
+			$('#board').append("<div data-value='1' class='selected freesquare' id='sqfree'><div class='text'><br/>free space</div></div>");
+			$('#board').append("<div data-value='0' class='square' id='sq12'><div class='text'><br/>"+JSONBingo.squares[i].square+"</div></div>");
+		} else {
+			$('#board').append("<div data-value='0' class='square' id='sq"+i+"'><div class='text'><br/>"+JSONBingo.squares[i].square+"</div></div>");
+		}
+  }
+
+	$('div.square').tappable(function () {
+      $(this).toggleClass('selected');
+      if ($(this).data('value') == 1) {
+            //alert(event.target.id);
+      		$(this).data('value', 0); }
+      else {
+            //alert(event.target.id);
+      		$(this).data('value', 1); }
+      		
+         clickSnd.play();
+
+		var row1 = ($('#sq0').data('value')+$('#sq1').data('value')+$('#sq2').data('value')+$('#sq3').data('value')+$('#sq4').data('value'));
+		var row2 = ($('#sq5').data('value')+$('#sq6').data('value')+$('#sq7').data('value')+$('#sq8').data('value')+$('#sq9').data('value'));
+		var row3 = ($('#sq10').data('value')+$('#sq11').data('value')+$('#sqfree').data('value')+$('#sq12').data('value')+$('#sq13').data('value'));
+		var row4 = ($('#sq14').data('value')+$('#sq15').data('value')+$('#sq16').data('value')+$('#sq17').data('value')+$('#sq18').data('value'));	
+		var row5 = ($('#sq19').data('value')+$('#sq20').data('value')+$('#sq21').data('value')+$('#sq22').data('value')+$('#sq23').data('value'));			
+
+		var col1 = ($('#sq0').data('value')+$('#sq5').data('value')+$('#sq10').data('value')+$('#sq14').data('value')+$('#sq19').data('value'));
+		var col2 = ($('#sq1').data('value')+$('#sq6').data('value')+$('#sq11').data('value')+$('#sq15').data('value')+$('#sq20').data('value'));
+		var col3 = ($('#sq2').data('value')+$('#sq7').data('value')+$('#sqfree').data('value')+$('#sq16').data('value')+$('#sq21').data('value'));
+		var col4 = ($('#sq3').data('value')+$('#sq8').data('value')+$('#sq12').data('value')+$('#sq17').data('value')+$('#sq22').data('value'));	
+		var col5 = ($('#sq4').data('value')+$('#sq9').data('value')+$('#sq13').data('value')+$('#sq18').data('value')+$('#sq23').data('value'));			
+
+		var diag1 = ($('#sq0').data('value')+$('#sq6').data('value')+$('#sqfree').data('value')+$('#sq17').data('value')+$('#sq23').data('value'));	
+		var diag2 = ($('#sq4').data('value')+$('#sq8').data('value')+$('#sqfree').data('value')+$('#sq15').data('value')+$('#sq19').data('value'));	
+		
+		if (row1 == 5 || row2 == 5 || row3 == 5 || row4 == 5 || row5 == 5 || col1 == 5 || col2 == 5 || col3 == 5  || col4 == 5  || col5 == 5 || diag1 == 5 || diag2 == 5) {
+			$('#header').html("Winner");
+			$('#header').addClass("win");
+			
+         		winSnd.play();
+    		
+    	} else {
+			$('#header').html(headerText);
+			$('#header').removeClass("win");
+		}; 
+    });
+        
+});
+
+
+shuffle = function(v){
+    	for(var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
+    	return v;
+};
+
+/*! Normalized address bar hiding for iOS & Android (c) @scottjehl MIT License */
+(function( win ){
+	var doc = win.document;
+	
+	// If there's a hash, or addEventListener is undefined, stop here
+	if( !location.hash && win.addEventListener ){
+		
+		//scroll to 1
+		window.scrollTo( 0, 1 );
+		var scrollTop = 1,
+			getScrollTop = function(){
+				return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
+			},
+		
+			//reset to 0 on bodyready, if needed
+			bodycheck = setInterval(function(){
+				if( doc.body ){
+					clearInterval( bodycheck );
+					scrollTop = getScrollTop();
+					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+				}	
+			}, 15 );
+		
+		win.addEventListener( "load", function(){
+			setTimeout(function(){
+				//at load, if user hasn't scrolled more than 20 or so...
+				if( getScrollTop() < 20 ){
+					//reset to hide addr bar at onload
+					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+				}
+			}, 0);
+		} );
+	}
+})( this );
