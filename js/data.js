419d640bb17ce58f85917863df753d083020d861
michaellevesque <xizillia@gmail.com>
2012-08-24 13:34:45 -0300

    Update js/data.js

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 
-var headerText = "Bingo";
+var headerText = "Stuff Republicans Say";
 
 var footerText = "<a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Code available on github</a>";
 
@@ -10,35 +10,35 @@ var clickSnd = new Audio("audio/click.mp3");
 var winSnd = new Audio("audio/win.mp3");
 
 var JSONBingo = {"squares": [
-        {"square": "one"},
-        {"square": "two"},
-        {"square": "three"},
-        {"square": "four"},
-        {"square": "five"},
-        {"square": "six"},
-        {"square": "seven"},
-        {"square": "eight"},
-        {"square": "nine"},
-        {"square": "ten"},
-        {"square": "eleven"},
-        {"square": "twelve"},
-        {"square": "thirteen"},
-        {"square": "fourteen"},
-        {"square": "fifteen"},
-        {"square": "sixteen"},
-        {"square": "seventeen"},
-        {"square": "eighteen"},
-        {"square": "nineteen"},
-        {"square": "twenty"},
-        {"square": "twenty one"},
-        {"square": "twenty two"},
-        {"square": "twenty three"},
-        {"square": "twenty four"},    	
-        {"square": "twenty five"},    	
-        {"square": "twenty six"},    	
-        {"square": "twenty seven"}, 
-        {"square": "twenty eight"},    	
-        {"square": "twenty nine"},    	
-        {"square": "thirty"}    	
+        {"square": "Only Atheists can get AIDS."},
+        {"square": "There would be less rape if all women were homemakers."},
+        {"square": "You can't get pregnant from Christmas rape."},
+        {"square": "Widespread contraception use leads to increased incidence of rape."},
+        {"square": "If you read 50 Shades of Grey, your going to hell."},
+        {"square": "Dads who conceive children through rape should have full visitation rights."},
+        {"square": "Homosexuality is a choice."},
+        {"square": "Everyone would be tempted to be gay if we didn't shame homosexuality."},
+        {"square": "There was not rape before feminism."},
+        {"square": "Marriage cures AIDS."},
+        {"square": "Church Pedophilia scandals result from tolerance of homosexuals."},
+        {"square": "Girls who watch Girls will never be happy wives."},
+        {"square": "Women subconsciously want to be raped; they send chemical signals that trigger men."},
+        {"square": "If we teach kids to put condoms on bananas, we might as well teach them how to use stripper poles."},
+        {"square": "If a girl gets drunk at a party, well, you know, I'm just sayin'."},
+        {"square": "This just shows that FemiNazi agenda is false."},
+        {"square": "Want to stop date rape? Stop Dating."},
+        {"square": "You can't take back the night; you never had it in the first place."},
+        {"square": "Middle school health class turn girls into prostitutes."},
+        {"square": "The solution to rape in the military is to kick out the women."},
+        {"square": "You want to know that your fellow soldier's got you back, not that he's staring at it."},
+        {"square": "Feminism's secret goal is to see all Christian boys aborted."},
+        {"square": "Well she has had many prior sexual partners you know."},
+        {"square": "If we're going to teach kids safe sex, we might as well just inject them with AIDS."},            
+        {"square": "If we stopped all imports the US economy would be stronger"},    	
+        {"square": "Immigration is the problem, cheap labor corrupts."},    	
+        {"square": "China has spies in all US companies, stealing our ideas."}, 
+        {"square": "If your CEO is not republican he is a net drain on the economy."},    	
+        {"square": "Farm subsides are beneficial because they help the economy."},    	
+        {"square": "If Marijuana was legal all our kids would use it."} 	
     ]
 };
